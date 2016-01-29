6ba1cd2fd6302fc65fb5696a0df7f02d91c7cbe9
sujitsajja <sujitsajja@outlook.com>
2016-01-28 19:36:35 -0600

    Challenges 3 and 4 updated

---
+++ Challenge3/Maxsum/src/Maxsum.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+class Solution {
+    public int maxSubArray(final List<Integer> a) {
+        int maxSum = 0;
+        int sum = 0;
+        int size = a.size();
+        for(int i=0;i<size;i++)
+        {
+            sum += a.get(i);
+            if(sum<0)
+                sum = 0;
+            if(maxSum<sum)
+                maxSum = sum;
+        }
+        if(maxSum==0)
+            maxSum = Collections.max(a);
+        return maxSum;
+    }
+}
+
+public class Maxsum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements in list : ");
+        int n = sc.nextInt();
+        ArrayList<Integer> input = new ArrayList<>(n);
+        System.out.println("Enter the numbers seperated by space/new line");
+        for(int i=0;i<n;i++)
+            input.add(sc.nextInt());
+        Solution sol = new Solution();
+        System.out.println("The maximum sum in the sub array is "+sol.maxSubArray(input));
+    }
+}
---
+++ Challenge4/Plus1/src/Plus1.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Solution {
+    public ArrayList<Integer> plusOne(ArrayList<Integer> a) {
+        int size = a.size();
+        ArrayList<Integer> result = new ArrayList<>(size);
+        boolean carryFlag = true;
+        for(int i=size-1;i>=0;i--)
+        {
+            int number = a.get(i);
+            if (carryFlag)
+            {
+                number++;
+                carryFlag = false;
+                if(number<10)
+                    a.set(i,number);
+                else
+                {
+                    carryFlag = true;
+                    a.set(i,0);
+                    if(i==0){
+                        result.add(1);
+                        for(int j=0;j<size;j++)
+                            result.add(a.get(j));
+                        return result;
+                    }
+                }
+            }
+        }
+        carryFlag = true;
+        for(int i=0;i<size;i++)
+        {
+            int number = a.get(i);
+            if(number!=0)
+                carryFlag = false;
+            if(!carryFlag)
+                result.add(number);
+        }
+        return result;
+    }
+}
+
+public class Plus1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of digits : ");
+        int n = sc.nextInt();
+        ArrayList<Integer> input = new ArrayList<>(n);
+        System.out.println("Enter the digits starting from MSD seperated by space/new line");
+        for(int i=0;i<n;i++)
+            input.add(sc.nextInt());
+        Solution sol = new Solution();
+        System.out.println("The result is : "+sol.plusOne(input));
+    }
+}
