62163f2fb7de673d7de756faf786fded39eee4e7
sujitsajja <sujitsajja@outlook.com>
2016-03-09 09:36:24 -0600

    added description files and updated challenges

---
+++ FindingUnique/src/FindingUnique.java
@@ -0,0 +1,93 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+/**
+ * Place all the unique elements of an array at the beginning
+ * 
+ * @author SujitS
+ * 
+ * @version 1.0
+ * @since 2016-03-09
+ */
+
+public class FindingUnique {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        Integer[] input = new Integer[n];
+        for(int i=0;i<n;i++)
+            input[i] = in.nextInt();
+        int k = findDistinct(input);
+        System.out.println("The distinct elements of given input are :");
+        for(int i=0;i<k;i++)
+            System.out.print(input[i]+" ");
+    }
+
+    /**
+     * Function to move distinct elements to the front of the array
+     * 
+     * @param <T> Generic type
+     * @param input Array of elements
+     * 
+     * @return integer k which indicates there are k
+     *         distinct elements in the given input array
+     */
+    private static<T> int findDistinct(T[] input) {
+        // Store the processed distinct elements in a set for future comparisions
+        HashSet<T> distinct = new HashSet<>();
+        int j = input.length-1;
+        int i=0;
+        int setSize = 0;
+        // distinct elements will be placed at the begining till index i-1
+        // Unprocessed elements will be between i and j inclusive
+        // duplicates elements will be placed from index j+1 to the last
+        while(i<=j) {
+            distinct.add(input[i]);
+            // If the current element is not a duplicate
+            // then the size of the set will be increased
+            if(setSize!=distinct.size()){
+                setSize++;
+                i++;
+            }
+            // If the current element is a duplicate
+            // the size of set will not be changed as the
+            // duplicate element overwrites the previous one
+            else{
+                // Place the duplicate element at the last of the array
+                swap(input,i,j);
+                j--;
+            }
+        }
+        return i;
+    }
+
+    /**
+     * Helper function to swap two elements in an array given their index positions
+     * 
+     * @param <T> Generic type
+     * @param input Array of elements
+     * @param i index of element to be swapped
+     * @param j index of element to be swapped with
+     */
+    private static <T> void swap(T[] input, int i, int j) {
+        T temp = input[i];
+        input[i] = input[j];
+        input[j] = temp;
+    }
+    
+}
+
+/**
+ * Sample Input :
+ * 20
+ * 1 2 3 2 5 2 3 1 4 2 4 2 6 7 3 8 7 1 9 5
+ * 
+ * Output :
+ * The distinct elements of given input are :
+ * 1 2 3 5 9 7 8 6 4
+ * 
+ */
---
+++ MostFrequent/src/MostFrequent.java
@@ -0,0 +1,111 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Scanner;
+
+/**
+ * Find most frequently repeated integer in an array
+ * with and without using hashing
+ * 
+ * @author SujitS
+ * 
+ * @version 1.0
+ * @since 2016-03-09
+ */
+
+public class MostFrequent {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] input = new int[n];
+        for(int i=0;i<n;i++)
+            input[i] = in.nextInt();
+        System.out.println("The most frequently repeating number(using hashmap) is : "+findMostRepeating(input));
+        System.out.println("The most frequently repeating number(without using hashmap) is : "+findMostRepeatingBySorting(input));
+    }
+
+    /**
+     * Function to find the most repeated number using HashMap
+     * 
+     * @param input Array of integers
+     * 
+     * @return integer which is most frequently repeated in input
+     */
+    private static int findMostRepeating(int[] input) {
+        HashMap<Integer,Integer> hmap = new HashMap<>();
+        for(Integer in: input){
+            // If it is not present previously we add it to the map
+            if(!hmap.containsKey(in))
+                hmap.put(in, 1);
+            // If it is already present we just increment its frequency
+            else
+                hmap.put(in, hmap.get(in)+1);
+        }
+        Iterator it = hmap.keySet().iterator();
+        int mostRepeatingNumber = 0;
+        int maxFrequency = 0;
+        while(it.hasNext()){
+            int number = (int) it.next();
+            int frequency = hmap.get(number);
+            if(frequency>maxFrequency){
+                mostRepeatingNumber = number;
+                maxFrequency = frequency;
+            }
+        }
+        return mostRepeatingNumber;
+    }
+
+    /**
+     * Function to find the most repeated number using sorting
+     * 
+     * @param input Array of integers
+     * 
+     * @return integer which is most frequently repeated in input
+     */
+    private static int findMostRepeatingBySorting(int[] input) {
+        // Sorting the input array
+        Arrays.sort(input);
+        int mostRepeatingNumber = 0;
+        int maxFrequency = 0;
+        int current = input[0];
+        int tempFrequency = 0;
+        for(int i=0;i<input.length;i++){
+            if(current == input[i])
+                tempFrequency++;
+            else{
+                if(tempFrequency>maxFrequency){
+                    maxFrequency = tempFrequency;
+                    mostRepeatingNumber = current;
+                }
+                current = input[i];
+                tempFrequency = 1;
+            }
+        }
+        if(tempFrequency>maxFrequency)
+            return current;
+        else
+            return mostRepeatingNumber;
+    }
+    
+}
+
+/**
+ * Sample Input:
+ * 20
+ * 1 2 3 2 5 2 3 1 4 2 4 2 6 7 3 8 7 1 9 5
+ * 
+ * Output:
+ * The most frequently repeating number(using hashmap) is : 2
+ * The most frequently repeating number(using sorting) is : 2
+ * 
+ * Running time analysis in ms:
+ * InputSize    UsingHashMap    UsingSorting
+ * 354175           72              70
+ * 820500           84              100
+ * 1440000          144             176
+ * 5760000          420             527
+ */
