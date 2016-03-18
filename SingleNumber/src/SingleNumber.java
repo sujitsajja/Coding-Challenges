137405562f69ff48bf93dac383ae6400f9710bc6
sujitsajja <sujitsajja@outlook.com>
2016-03-18 14:43:18 -0500

    new Challenge added

---
+++ SingleNumber/src/SingleNumber.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+/**
+ * Given two strings check if they are anagrams or not
+ * 
+ * @author SujitS
+ * 
+ * @version 1.0
+ * @since 2016-03-18
+ */
+
+public class SingleNumber {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
+
+/**
+ * 
+ */
