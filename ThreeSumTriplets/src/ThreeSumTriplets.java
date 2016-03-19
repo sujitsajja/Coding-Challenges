852de87b4d5c48c69456b9df98d3672c10652442
sujitsajja <sujitsajja@outlook.com>
2016-03-19 09:48:00 -0500

    new challenge added

---
+++ ThreeSumTriplets/src/ThreeSumTriplets.java
@@ -0,0 +1,24 @@
+/**
+ * Given an array find all the triplets whose
+ * sum is equal to the given target.
+ * 
+ * @author SujitS
+ * 
+ * @version 1.0
+ * @since 2016-03-19
+ */
+
+public class ThreeSumTriplets {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
+
+/**
+ * 
+ */
