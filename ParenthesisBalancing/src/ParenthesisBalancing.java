540d8dbcadee455012043630b03d2e4a3086dabe
sujitsajja <sujitsajja@outlook.com>
2016-03-23 15:26:40 -0500

    Challenge completed

--- ParenthesisBalancing/src/ParenthesisBalancing.java
+++ ParenthesisBalancing/src/ParenthesisBalancing.java
@@ -1,3 +1,6 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
 /**
  * To check if the given order of parenthesis is balanced or not
  * 
@@ -13,9 +16,58 @@ public class ParenthesisBalancing {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Parenthesis to be checked for balance : ");
+        String input = sc.next();
+        if(isBalanced(input))
+            System.out.println("The given parenthesis is balanced");
+        else
+            System.out.println("The given parenthesis is not balanced");
     }
-    
+
+    /**
+     * Function to check if the given parenthesis is balanced or not
+     * 
+     * @param input String representation of parenthesis
+     * 
+     * @return True : If the parenthesis is balanced
+     *         False: If the parenthesis is not balanced
+     */
+    private static boolean isBalanced(String input) {
+        int length = input.length();
+        if(length%2!=0)
+            return false;
+        ArrayDeque<Character> openParenthesis = new ArrayDeque<>();
+        Character openBracket = '(';
+        Character openSquare = '[';
+        Character openFlower = '{';
+        Character closeBracket = ')';
+        Character closeSquare = ']';
+        Character closeFlower = '}';
+        for(int i=0;i<length;i++){
+            Character current = input.charAt(i);
+            if(current.equals(openBracket) || 
+                    current.equals(openFlower) || 
+                    current.equals(openSquare))
+                openParenthesis.push(current);
+            else{
+                if(current.equals(closeBracket)){
+                    if(!openParenthesis.pop().equals(openBracket))
+                        return false;
+                }
+                else if(current.equals(closeFlower)){
+                    if(!openParenthesis.pop().equals(openFlower))
+                        return false;
+                }
+                else if(current.equals(closeSquare)){
+                    if(!openParenthesis.pop().equals(openSquare))
+                        return false;
+                }
+            }
+        }
+        return openParenthesis.isEmpty();
+    }
+
 }
 
 /**
