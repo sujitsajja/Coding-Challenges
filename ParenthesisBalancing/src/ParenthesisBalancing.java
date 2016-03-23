a2430233d0dd8faced04c480338c8435ef0c820e
sujitsajja <sujitsajja@outlook.com>
2016-03-23 15:10:00 -0500

    Added challenge description

--- ParenthesisBalancing/src/ParenthesisBalancing.java
+++ ParenthesisBalancing/src/ParenthesisBalancing.java
@@ -1,13 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * To check if the given order of parenthesis is balanced or not
+ * 
+ * @author SujitS
+ * 
+ * @version 1.0
+ * @since 2016-03-21
  */
 
-/**
- *
- * @author SujitS
- */
 public class ParenthesisBalancing {
 
     /**
@@ -18,3 +17,7 @@ public class ParenthesisBalancing {
     }
     
 }
+
+/**
+ * 
+ */
