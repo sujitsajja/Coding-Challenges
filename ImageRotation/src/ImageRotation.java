16ce0c3a63d411c05be45bf004e7a563543e1bec
sujitsajja <sujitsajja@outlook.com>
2016-03-29 13:15:42 -0500

    Challenge completed

--- ImageRotation/src/ImageRotation.java
+++ ImageRotation/src/ImageRotation.java
@@ -1,20 +1,61 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.util.Scanner;
+
 
 /**
- *
+ * To rotate an image represented as NxN matrix by 90 degrees
+ * 
  * @author SujitS
+ * 
+ * @version 1.0
+ * @since 2016-03-29
  */
+
 public class ImageRotation {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of image : ");
+        int n = sc.nextInt();
+        int[][] image = new int[n][n];
+        System.out.println("Enter the elements of image seperated by space/enter :");
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++)
+                image[i][j] = sc.nextInt();
+        }
+        System.out.println("The image before rotation :");
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++)
+                System.out.print(image[i][j]+" ");
+            System.out.println();
+        }
+        rotateImage(image);
+        System.out.println("The image after rotation :");
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++)
+                System.out.print(image[i][j]+" ");
+            System.out.println();
+        }
     }
-    
+
+    private static void rotateImage(int[][] image) {
+        int n = image.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i;j++){
+                int temp = image[i][j];
+                image[i][j] = image[n-j-1][n-i-1];
+                image[n-j-1][n-i-1] = temp;
+            }
+        }
+        for(int i=0;i<n/2;i++){
+            for(int j=0;j<n;j++){
+                int temp = image[i][j];
+                image[i][j] = image[n-i-1][j];
+                image[n-i-1][j] = temp;
+            }
+        }
+    }
+
 }
