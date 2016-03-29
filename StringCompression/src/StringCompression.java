10102893549456b7ca93b581a8832e2389976f4d
sujitsajja <sujitsajja@outlook.com>
2016-03-29 11:48:42 -0500

    Challenge completed

--- StringCompression/src/StringCompression.java
+++ StringCompression/src/StringCompression.java
@@ -1,20 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.util.Scanner;
 
 /**
- *
+ * To compress the given string
+ * 
  * @author SujitS
+ * 
+ * @version 1.0
+ * @since 2016-03-29
  */
+
 public class StringCompression {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the string to be compressed : ");
+        String input = sc.next();
+        String output = stringCompress(input);
+        System.out.println("The string after compression is : "+output);
+    }
+
+    private static String stringCompress(String input) {
+        int length = input.length();
+        int count = 0;
+        StringBuilder output = new StringBuilder();
+        char curr = input.charAt(0);
+        for(int i=0;i<length;i++){
+            char temp = input.charAt(i);
+            if(curr == temp)
+                count++;
+            else{
+                output.append(curr);
+                output.append(count);
+                count = 1;
+                curr = temp;
+            }
+        }
+        output.append(curr);
+        output.append(count);
+        return output.toString();
     }
     
 }
