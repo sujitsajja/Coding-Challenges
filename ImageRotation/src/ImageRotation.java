10b9301530251e39e62ec049b5476683c2d5a127
sujitsajja <sujitsajja@outlook.com>
2016-03-29 13:25:35 -0500

    Sample input and output included

--- ImageRotation/src/ImageRotation.java
+++ ImageRotation/src/ImageRotation.java
@@ -73,3 +73,18 @@ public class ImageRotation {
         image[x2][y2] = temp;
     }
 }
+
+/**
+ * Sample input:
+ * 4
+ * 1  2  3  4
+ * 5  6  7  8
+ * 9  10 11 12
+ * 13 14 15 16
+ * 
+ * Sample output:
+ * 13 9  5 1
+ * 14 10 6 2
+ * 15 11 7 3
+ * 16 12 8 4 
+ */
