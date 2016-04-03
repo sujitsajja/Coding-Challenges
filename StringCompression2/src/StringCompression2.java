66bc4139bdca02522adda8987cc48cda8a7f6b6c
sujitsajja <sujitsajja@outlook.com>
2016-04-03 13:37:56 -0500

    New challenge added

---
+++ StringCompression2/src/StringCompression2.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ * 
+ * @author SujitS
+ * 
+ * @version 1.0
+ * @since 2016-04-03
+ */
+
+public class StringCompression2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
