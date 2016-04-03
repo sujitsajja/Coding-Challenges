c40d2506c73b3ad8e4431771c724cadc7a5a6b16
sujitsajja <sujitsajja@outlook.com>
2016-04-03 14:03:50 -0500

    included comments

--- StringCompression2/src/StringCompression2.java
+++ StringCompression2/src/StringCompression2.java
@@ -22,22 +22,45 @@ public class StringCompression2 {
         System.out.println("The string after compression is : "+output);
     }
 
+    /**
+     * Function to compress the string
+     * 
+     * @param input String to be compressed
+     * 
+     * @return Compressed string
+     */
     private static String compress(String input) {
         char first = input.charAt(0);
         int count = 1;
         return compress(input,first,count,1).toString();
     }
 
+    /**
+     * Recursive function to compress the string
+     * 
+     * @param input String to be compressed
+     * @param curr Current character
+     * @param count Number of times the current character is repeated
+     * @param index Position of string currently considered
+     * 
+     * @return Compressed String
+     */
     private static StringBuilder compress(String input, char curr, int count, int index) {
         StringBuilder current = new StringBuilder();
+        // If end of string is reached
         if(input.length()==index){
             current.append(curr);
             current.append(count);
             return current;
         }
         char temp = input.charAt(index);
+        // If current char is same as previous
+        // then we just increment the pointer and counter
         if(temp == curr)
             return compress(input, curr, count+1, index+1);
+        // If current char is not same as previous
+        // then we compress the previous one
+        // and recursively compress the remaining
         else{
             current.append(curr);
             current.append(count);
