db34465b8361ead98e7fdbb83b9c4fc4d500ae27
sujitsajja <sujitsajja@outlook.com>
2016-04-15 19:51:35 -0500

    New Challenge added

---
+++ SlidingWindowMaximum/src/SlidingWindowMaximum.java
@@ -0,0 +1,20 @@
+/**
+ * To find the maximum number in the window of
+ * given size which slides from start till end
+ * 
+ * @author SujitS
+ * 
+ * @version 1.0
+ * @since 2016-04-15
+ */
+
+public class SlidingWindowMaximum {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
